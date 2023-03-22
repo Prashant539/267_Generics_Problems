0d083e89cc10673b15ff805c7b80ae86929786f2
Prashant <prashant.chaurasiya.cp@gmail.com>
2023-03-22 11:47:32 +0530

    [Prashant]Add: Performing deleting array element

---
+++ GenericsDemo/GenericsDemo/DeleteArrayElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    public class DeleteArrayElement
+    {
+        public static void DeleteElement(int[] arr, int delete)
+        {
+            int k = 0;
+            int[] result = new int[arr.Length - 1];
+            for(int i = 0; i< arr.Length; i++)
+            {
+                if (delete != arr[i])
+                {
+                    result[k] = arr[i];
+                    k++;
+                }
+            }
+            foreach(var j in result)
+            {
+                Console.WriteLine(j + " ");
+            }
+        }
+    }
+}
---
+++ GenericsDemo/GenericsDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace GenericsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many elements to add into array ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            Console.WriteLine("Plese enter element one by one");
+            for(int i = 0; i <arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("\nPlase pickup any one element from this array");
+            int delete = Convert.ToInt32(Console.ReadLine());
+            DeleteArrayElement.DeleteElement(arr, delete);
+            Console.ReadLine();
+        }
+    }
+}
